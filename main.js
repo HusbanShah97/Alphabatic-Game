777abf49055fa4a686fc4cd6306b0595defd61fe
HusbanShah97 <husbanshah97@gmail.com>
2024-07-20 15:16:06 +0500

    initail commit

---
+++ main.js
@@ -0,0 +1,33 @@
+
+
+document.getElementById("text");
+
+function a() {
+
+    let arr =  [`Apple`, `Aeroplane`, `Ant`, 'Arrow', 'Artice', 'Art']
+    let random = Math.floor(Math.random() * 6)
+    text.innerText = arr[random];
+
+
+    }
+    
+    
+    
+    
+
+
+function b() {
+
+    let arr = [`Banana`, `Ball`, `Batman`, 'Bear' , 'Bee' , 'Bird'];
+    let random = Math.floor(Math.random() * 6)
+    text.innerText = arr[random];
+    
+}
+
+function c() {
+    let arr = ['cat', 'cort', 'crown', 'catch', 'clothe', 'clone']
+    let random = Math.floor(Math.random() * 6)
+    text.innerText = arr[random]
+console.log(random);
+    
+} 
